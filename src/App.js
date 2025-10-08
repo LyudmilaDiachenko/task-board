a6e96dabacf2717fa8979351ce719c1c445de084
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-10-08 22:03:35 +0300

    add task form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import './App.css';
 import Columns from "./js/columns";
-
+import AddTask from "./js/addTask";
 
 function App() {
+  const [tasks, setTasks] = useState([])
   const [columns, setColumns] = useState(
     ['Pending', 'In progress', 'Done']
   )
 
   return (
     <div className="App">
-      <Columns {...{columns, setColumns}}/>
+      <Columns {...{columns, setColumns}} />
+      <AddTask  {...{tasks, setTasks}} />
     </div>
   );
 }
---
+++ src/js/addTask.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+
+function AddTask({tasks, setTasks}) {
+    const [task, setTask] = useState({})
+
+    return (
+        <div>
+            <form className="add-task-form">
+                <label className="add-task-lable">
+                    <input
+                        className="add-task-input"
+                        type="text" 
+                        placeholder="Title" 
+                        value={task?.title || ''} 
+                        onChange={e => setTask({...task, title: e.target.value})}
+                    />
+                </label>
+                <label className="add-task-lable">
+                    <textarea 
+                        className="add-task-textarea"
+                        placeholder="Description"
+                        value={task?.description || ''}
+                        onChange={e => setTask({...task, description: e.target.value})}
+                    >
+                    </textarea>
+                </label>
+                <div className="select-date-box">
+                    <label>
+                        <select className="add-task-select" onChange={e => setTask({...task, assignee: e.target.value})}>
+                            <option disabled checked>Select assignee</option>
+                            <option value="anna">Anna Smith</option>
+                            <option value="john">John Doe</option>
+                            <option value="emma">Emma Johnson</option>
+                        </select>
+                    </label>
+                    <label>
+                        <input className="add-task-date" type="date"  onChange={e => setTask({...task, deadline: e.target.value})} />
+                    </label>
+                </div>
+                <button 
+                    className="add-task-btn" 
+                    type="button" 
+                    onClick={_ => setTasks([...tasks, task]) || setTask({})}
+                >
+                    Save Task
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTask;
